915376fe836ede6c15778f5a1903fd114642bd62
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2023-12-10 21:45:15 +0530

    About page is on dark mode

--- src/app/about/AboutSection.tsx
+++ src/app/about/AboutSection.tsx
@@ -10,38 +10,38 @@ export default function AboutSection({
   achievements}) {
   return (
     <div>
-      <div className="bg-white">
+      <div className="bg-black">
         <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:gap-8">
             <div className="lg:self-center">
-              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+              <h2 className="text-3xl font-extrabold text-gray-200 sm:text-4xl">
                 About Me
               </h2>
-              <p className="mt-4 text-lg text-gray-500">{para}</p>
+              <p className="mt-4 text-lg text-gray-400">{para}</p>
               <div className="flex mt-8">
                 <div className="flex-shrink-0">
-                  <div className="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
+                  <div className="flex items-center justify-center w-12 h-12 text-white bg-blue-500 rounded-md">
                     <FaGraduationCap className="w-6 h-6" />
                   </div>
                 </div>
                 <div className="ml-4">
-                  <h3 className="text-xl font-bold text-gray-900">Education</h3>
-                  <p className="mt-2 text-base text-gray-500">
+                  <h3 className="text-xl font-bold text-gray-300">Education</h3>
+                  <p className="mt-2 text-base text-gray-400">
                     {degree}
                   </p>
                 </div>
               </div>
               <div className="flex mt-6">
                 <div className="flex-shrink-0">
-                  <div className="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
+                  <div className="flex items-center justify-center w-12 h-12 text-white bg-blue-500 rounded-md">
                     <GiTrophyCup className="w-6 h-6" />
                   </div>
                 </div>
                 <div className="ml-4">
-                  <h3 className="text-xl font-bold text-gray-900">
+                  <h3 className="text-xl font-bold text-gray-300">
                     Achievements
                   </h3>
-                  <ul className="mt-2 text-base text-gray-500 list-disc list-inside">
+                  <ul className="mt-2 text-base text-gray-400 list-disc list-inside">
                     {achievements.slice(0,1).map((item: string ) =><li key={item}>{item}</li>)}
                   </ul>
                 </div>
--- src/app/about/FollowSection.tsx
+++ src/app/about/FollowSection.tsx
@@ -6,58 +6,38 @@ import { BsSkype, BsTelephoneFill, BsWhatsapp } from 'react-icons/bs';
 const FollowUs = ({socialMediaLinks}) => {
 
   return (
-    <div className="py-16">
+    <div className="py-16 bg-black">
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="text-center">
-          <h2 className="text-base font-semibold tracking-wide text-indigo-600 uppercase">Follow Us</h2>
-          <p className="mt-1 text-3xl font-extrabold text-gray-900 sm:text-4xl sm:tracking-tight lg:text-5xl">Stay Connected</p>
-          <p className="max-w-xl mx-auto mt-5 text-gray-500 text-md">Follow us on social media to stay up-to-date with our latest news and announcements.</p>
+          <h2 className="text-base font-semibold tracking-wide text-blue-500 uppercase">Follow Me</h2>
+          <p className="mt-1 text-3xl font-extrabold text-gray-200 sm:text-4xl sm:tracking-tight lg:text-5xl">Stay Connected</p>
+          <p className="max-w-xl mx-auto mt-5 text-gray-400 text-md">Follow us on social media to stay up-to-date with our latest news and announcements.</p>
         </div>
         <div className="mt-12">
           <div className="flex items-center justify-center gap-20">
-            <a href={socialMediaLinks.twitter}  className="text-gray-400 hover:text-gray-800">
+            <a href={socialMediaLinks.twitter}  className="text-gray-200 hover:text-gray-400">
               <span className="sr-only">Twitter</span>
               <FaTwitter className="w-8 h-8 mx-auto" />
             </a>
 
-            <a href={socialMediaLinks.github} className="text-gray-400 hover:text-gray-800">
+            <a href={socialMediaLinks.github} className="text-gray-200 hover:text-gray-400">
               <span className="sr-only">Github</span>
               <FaGithub className="w-8 h-8 mx-auto" />
             </a>
 
-            <a href={socialMediaLinks.linkedIn} className="text-gray-400 hover:text-gray-800">
+            <a href={socialMediaLinks.linkedIn} className="text-gray-200 hover:text-gray-400">
               <span className="sr-only">LinkedIn</span>
               <FaLinkedin className="w-8 h-8 mx-auto" />
             </a>
-            <a href={socialMediaLinks.whatsApp} className="text-gray-400 hover:text-gray-800">
+            <a href={socialMediaLinks.whatsApp} className="text-gray-200 hover:text-gray-400">
               <span className="sr-only">WhatsApp</span>
               <BsWhatsapp className="w-8 h-8 mx-auto" />
             </a>
 
-            {/* <a href={socialMediaLinks.instagram} className="text-gray-400 hover:text-gray-800">
-              <span className="sr-only">Instagram</span>
-              <FaInstagram className="w-8 h-8 mx-auto" />
-            </a> */}
-
-            {/* <a href={socialMediaLinks.facebook} className="text-gray-400 hover:text-gray-800">
-              <span className="sr-only">Facebook</span>
-              <FaFacebook className="w-8 h-8 mx-auto" />
-            </a> */}
-
-            {/* <a href={socialMediaLinks.skype} className="text-gray-400 hover:text-gray-800">
-              <span className="sr-only">Skype</span>
-              <BsSkype className="w-8 h-8 mx-auto" />
-            </a> */}
-
-            <a href={`mailto:${socialMediaLinks.mail}`} className="text-gray-400 hover:text-gray-800">
+            <a href={`mailto:${socialMediaLinks.mail}`} className="text-gray-200 hover:text-gray-400">
               <span className="sr-only">Mail</span>
               <MdEmail className="w-8 h-8 mx-auto" />
             </a>
-
-            {/* <a href={`tel:${socialMediaLinks.tel}`} className="text-gray-400 hover:text-gray-800">
-              <span className="sr-only">Phone</span>
-              <BsTelephoneFill className="w-8 h-8 mx-auto" />
-            </a> */}
           </div>
         </div>
       </div>
