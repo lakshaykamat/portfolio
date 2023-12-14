8fed49a11caa3ad9ecc470a24726aae7804a3c49
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2023-12-14 22:06:36 +0530

    FIX: Footer icons

--- src/app/components/Common/Footer.tsx
+++ src/app/components/Common/Footer.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-import { FaEnvelope, FaPhoneAlt } from "react-icons/fa";
-import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi";
-import { BsWhatsapp} from "react-icons/bs";
+import { FaEnvelope ,FaGithub, FaLinkedin, FaTwitter} from "react-icons/fa";
 import { socialMediaLinks } from "../../../data";
-const Item = ({ link, text, jsx }) => {
+
+const SocialMediaIcon = ({ link, text, jsx }) => {
   return (
     <li className="m-2 text-center">
       <a
@@ -12,45 +10,35 @@ const Item = ({ link, text, jsx }) => {
         rel="noopener noreferrer"
         className="hover:text-white"
       >
-        <div className="flex items-center justify-center gap-2">
+        <div className="">
           {jsx}
-          <h5>{text}</h5>
+          {/* <h5>{text}</h5> */}
         </div>
       </a>
     </li>
   );
 };
-const data = [
+const socialMediaConfig = [
   {
     link: `mailto:${socialMediaLinks.mail}`,
     text: "Email",
     jsx: <FaEnvelope />,
   },
-  // {
-  //   link: `tel:${socialMediaLinks.tel}`,
-  //   text: "Phone",
-  //   jsx: <FaPhoneAlt />,
-  // },
   {
     link: socialMediaLinks.github,
     text: "Github",
-    jsx: <FiGithub />,
+    jsx:<FaGithub/>,
   },
   {
     link: socialMediaLinks.linkedIn,
     text: "LinkedIn",
-    jsx: <FiLinkedin />,
+    jsx:<FaLinkedin/>,
   },
   {
     link: socialMediaLinks.twitter,
     text: "Twitter",
-    jsx: <FiTwitter />,
+    jsx: <FaTwitter/>,
   },
-  {
-    link:socialMediaLinks.whatsApp,
-    text:"Whatsapp",
-    jsx:<BsWhatsapp/>
-  }
 ];
 const Footer = () => {
   return (
@@ -61,8 +49,8 @@ const Footer = () => {
         </div>
         <div className="flex items-center">
           <ul className="flex flex-wrap justify-center space-x-7">
-            {data.map((item,index) => {
-              return <Item key={index} link={item.link} text={item.text} jsx={item.jsx} />;
+            {socialMediaConfig.map((item,index) => {
+              return <SocialMediaIcon key={index} link={item.link} text={item.text} jsx={item.jsx} />;
             })}
           </ul>
         </div>
