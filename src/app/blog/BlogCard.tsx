f33f351be65b4bb5c5c8dacb8926e7edf1da662c
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2023-12-15 16:18:23 +0530

    Create Blog and add not found page

--- src/app/blog/BlogCard.tsx
+++ src/app/blog/BlogCard.tsx
@@ -4,10 +4,10 @@ import getFormattedDate from "../../lib/getFormattedDate"
 
 const BlogCard = (props: BlogPost) => {
   return (
-    <div className="px-5 py-3 rounded bg-slate-800 drop-shadow-lg">
+    <div className="flex flex-col items-start px-5 py-3 rounded bg-slate-800 drop-shadow-lg">
         <h2 className="my-2 text-2xl font-semibold">{props.title}</h2>
         <span className="text-gray-400">{getFormattedDate(props.date)}</span>
-        <p className="my-2 mb-6">{props.description}</p>
+        <p className="flex-1 my-2 mb-6 text-gray-300">{props.description}</p>
         <Link href={`/blog/${props.id}`} className="mb-6 btn-blue-500">Read more
         </Link>
     </div>
